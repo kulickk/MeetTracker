03adfd8a960d9539cc6917119703283d7fee02ce
kulickk <lilrik1328@gmail.com>
2024-10-22 19:18:09 +0500

    added env variables for JWT generation

--- src/config.py
+++ src/config.py
@@ -9,3 +9,6 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
