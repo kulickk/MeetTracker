1565b1c86629f104557eeb893dae8c88d75fb112
MaT0kz <98161722+MaT0kz@users.noreply.github.com>
2024-11-13 01:26:39 +0500

    Add files via upload

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,8 +1,18 @@
+const dommen = 'http://localhost:8000/';
+
 const api = {
-    register: 'http://localhost:8000/auth/register',
-    test: 'http://127.0.0.1:8000/auth/health',
-    users: 'http://127.0.0.1:8000/auth/users/me',
-    logIn: 'http://127.0.0.1:8000/auth/token'
+    register: 'auth/register',
+    test: 'auth/health',
+    users: 'auth/users/me',
+    logIn: 'auth/token',
+    adminGetUsers: 'admin/get_users',
+    adminBanUser: 'admin/ban_user',
+    adminMakeUserAdmin: 'admin/set_admin',
+    adminRegisterUser: 'admin/register',
 };
+
+Object.keys(api).forEach((key) => {
+    api[key] = dommen + api[key];
+});
 
 export default api;
--- my-app/src/pages/MainPage/MainPage.jsx
+++ my-app/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,20 @@
+import api from "../../api";
+
+
 const MainPage = (props) => {
     return (
         <div>
             ГЛАВНАЯ СТРАНИЦА
-            <button onClick={props.handleLogOut} >LogOut</button>
+            <button onClick={props.handleLogOut}>LogOut</button>
             {props.token}
+            <button onClick={
+                e => {
+                    fetch(api.users, {
+                        method: 'GET',
+                        credentials: 'include',
+                    })
+                }
+            }>User</button>
         </div>
     );
 };
