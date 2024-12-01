4c059bc967265566684df54bec651bdf2ec2f914
kulickk <lilrik1328@gmail.com>
2024-12-01 19:17:57 +0500

    added UpdateUserInfo schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -57,6 +57,32 @@ class UserReadSchema(UserBaseSchema):
         from_attributes = True
 
 
+class UpdateUserInfo(BaseModel):
+    old_password: str | None = Field(None, description="Старый пароль", min_length=8)
+    new_password: str | None = Field(None, description="Новый пароль", min_length=8)
+    name: str | None = Field(
+        None,
+        min_length=2,
+        max_length=20,
+        pattern=r'^[А-ЯЁ][а-яё]+$',
+        description="Имя пользователя должно содержать только русские буквы."
+    )
+    surname: str | None = Field(
+        None,
+        min_length=2,
+        max_length=20,
+        pattern=r'^[А-ЯЁ][а-яё]+$',
+        description="Имя пользователя должно содержать только русские буквы."
+    )
+    patronymic: str | None = Field(
+        None,
+        min_length=2,
+        max_length=20,
+        pattern=r'^^[А-ЯЁ][а-яё]+$',
+        description="Имя пользователя должно содержать только русские буквы."
+    )
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
