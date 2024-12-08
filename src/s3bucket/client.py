524b65292cce907b88cd5ff3ea7dc9df10c9a87d
kulickk <lilrik1328@gmail.com>
2024-12-08 21:19:32 +0500

    added get_file from s3bucket

--- src/s3bucket/client.py
+++ src/s3bucket/client.py
@@ -1,7 +1,5 @@
 from contextlib import asynccontextmanager
-
 from aiobotocore.session import get_session
-
 from src.config import S3_SECRET_KEY, S3_ACCESS_KEY, S3_ENDPOINT_URL, S3_BUCKET_NAME
 
 
@@ -21,19 +19,16 @@ class S3Client:
         async with self.session.create_client('s3', **self.config) as s3_client:
             yield s3_client
 
-    async def upload_file(self, file_path: str):
-        object_name = file_path.split('/')[-1]
+    async def upload_file(self, file_name, data):
+        object_name = file_name
         async with self.get_client() as s3_client:
-            with open(file_path, 'rb') as data:
-                await s3_client.put_object(Bucket=self.bucket_name, Key=object_name, Body=data)
+            await s3_client.put_object(Bucket=self.bucket_name, Key=object_name, Body=data)
 
-    # async def get_file(self, file_path: str):
-    #     object_name = file_path.split('/')[-1]
-    #     async with self.get_client() as s3_client:
-    #         result = await s3_client.get_object(Bucket=self.bucket_name, Key=object_name)
-    #         return result
-
-    # async def del_file(self, file_path: str):
-    #     object_name = file_path.split('/')[-1]
-    #     async with self.get_client() as s3_client:
-    #         await s3_client.delete_file(Bucket=self.bucket_name, Key=object_name)
+    async def get_file(self, file_name: str):
+        object_name = file_name
+        async with self.get_client() as s3_client:
+            resp = await s3_client.get_object(Bucket=self.bucket_name, Key=object_name)
+            async with resp['Body'] as stream:
+                data = await stream.read()
+                data_type = resp['ResponseMetadata']['HTTPHeaders']['content-type']
+                return data, data_type
